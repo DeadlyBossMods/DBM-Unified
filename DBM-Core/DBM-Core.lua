fd233d858e1cf795eeffe0156773824fa576ff5f
MysticalOS <mysticalosx@gmail.com>
2021-09-22 11:30:42 -0400

    Bump alpha revisions

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -71,10 +71,10 @@ if isRetail then
 	DBM.DisplayVersion = "9.1.15 alpha"
 	DBM.ReleaseRevision = releaseDate(2021, 9, 21) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 elseif isClassic then
-	DBM.DisplayVersion = "1.13.81"
+	DBM.DisplayVersion = "1.13.82 alpha"
 	DBM.ReleaseRevision = releaseDate(2021, 9, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 elseif isBCC then
-	DBM.DisplayVersion = "2.5.14"
+	DBM.DisplayVersion = "2.5.15 alpha"
 	DBM.ReleaseRevision = releaseDate(2021, 9, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 end
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
