7f0cfb87f888d1feb7ec0821bdfb721a7a394509
MysticalOS <mysticalosx@gmail.com>
2021-09-22 10:14:38 -0400

    increment classic versions

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -71,11 +71,11 @@ if isRetail then
 	DBM.DisplayVersion = "9.1.15 alpha"
 	DBM.ReleaseRevision = releaseDate(2021, 9, 21) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 elseif isClassic then
-	DBM.DisplayVersion = "1.13.80 alpha"
-	DBM.ReleaseRevision = releaseDate(2021, 9, 21) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DBM.DisplayVersion = "1.13.80"
+	DBM.ReleaseRevision = releaseDate(2021, 9, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 elseif isBCC then
-	DBM.DisplayVersion = "2.5.14 alpha"
-	DBM.ReleaseRevision = releaseDate(2021, 9, 21) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DBM.DisplayVersion = "2.5.14"
+	DBM.ReleaseRevision = releaseDate(2021, 9, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 end
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
