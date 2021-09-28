8ed20497205f553a4d6d259a00be6265b5bd8ad9
MysticalOS <mysticalosx@gmail.com>
2021-09-27 20:45:14 -0400

    prepare new tags

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -68,14 +68,14 @@ DBM = {
 }
 -- The string that is shown as version
 if isRetail then
-	DBM.DisplayVersion = "9.1.15 alpha"
-	DBM.ReleaseRevision = releaseDate(2021, 9, 21) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DBM.DisplayVersion = "9.1.15"
+	DBM.ReleaseRevision = releaseDate(2021, 9, 27) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 elseif isClassic then
-	DBM.DisplayVersion = "1.13.82 alpha"
-	DBM.ReleaseRevision = releaseDate(2021, 9, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DBM.DisplayVersion = "1.13.82"
+	DBM.ReleaseRevision = releaseDate(2021, 9, 27) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 elseif isBCC then
-	DBM.DisplayVersion = "2.5.15 alpha"
-	DBM.ReleaseRevision = releaseDate(2021, 9, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DBM.DisplayVersion = "2.5.15"
+	DBM.ReleaseRevision = releaseDate(2021, 9, 27) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 end
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
