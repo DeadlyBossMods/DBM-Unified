331218276fca97737122c490a05bb3e547262967
MysticalOS <mysticalosx@gmail.com>
2022-02-22 06:57:10 -0500

    changing it back to spellId because that's only way i won't fuck it up

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -10102,11 +10102,11 @@ end
 
 -- FIXME: this function does not reset any settings to default if you remove an option in a later revision and a user has selected this option in an earlier revision were it still was available
 -- this will be fixed as soon as it is necessary due to removed options ;-)
-function bossModPrototype:AddDropdownOption(name, options, default, cat, func, spellID)
+function bossModPrototype:AddDropdownOption(name, options, default, cat, func, spellId)
 	cat = cat or "misc"
 	self.DefaultOptions[name] = {type = "dropdown", value = default}
 	self.Options[name] = default
-	if spellID then
+	if spellId then
 		self:GroupSpells(spellId, name)
 	end
 	self:SetOptionCategory(name, cat)
