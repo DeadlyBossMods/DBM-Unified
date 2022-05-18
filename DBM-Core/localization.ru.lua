e57f2dbc9f27f2b409704ecd6683cfcabdae20e3
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2022-05-18 14:31:51 +0500

    Update localization.ru.lua
    Translation of some phrases.

--- DBM-Core/localization.ru.lua
+++ DBM-Core/localization.ru.lua
@@ -472,7 +472,12 @@ L.AUTO_TIMER_OPTIONS.adds			= "Отсчёт времени до прибытия
 L.AUTO_TIMER_OPTIONS.addscustom		= "Отсчёт времени до прибытия аддов"
 L.AUTO_TIMER_OPTIONS.roleplay		= "Отсчёт времени для ролевой игры"
 
-L.AUTO_ICONS_OPTION_TARGETS			= "Устанавливать метки на цели заклинания $spell:%s"
+L.AUTO_ICONS_OPTION_TARGETS			= "Устанавливать метки на цели $spell:%s"
+L.AUTO_ICONS_OPTION_TARGETS_MELEE_A		= "Устанавливать метки на цели $spell:%s с приоритетом ближнего боя и в алфавитном порядке"
+L.AUTO_ICONS_OPTION_TARGETS_MELEE_R		= "Устанавливать метки на цели $spell:%s с приоритетом ближнего боя и рейдового состава"
+L.AUTO_ICONS_OPTION_TARGETS_RANGED_A	= "Устанавливать метки на цели $spell:%s с приоритетом дальнего боя и в алфавитном порядке"
+L.AUTO_ICONS_OPTION_TARGETS_RANGED_R	= "Устанавливать метки на цели $spell:%s с приоритетом дальнего боя и рейдового состава"
+L.AUTO_ICONS_OPTION_TARGETS_ALPHA		= "Устанавливать метки на цели $spell:%s в алфавитном порядке"
 L.AUTO_ICONS_OPTION_NPCS			= "Устанавливать метки на $spell:%s"
 L.AUTO_ICONS_OPTION_CONFLICT		= " (Может конфликтовать с другими параметрами)"
 L.AUTO_ARROW_OPTION_TEXT			= "Показывать стрелку " .. L.DBM .. " к цели, на которой $spell:%s"
