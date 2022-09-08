30179a9f9c77da970ddd93e51f731e494e7b626a
MysticalOS <mysticalosx@gmail.com>
2022-09-07 20:04:11 -0400

    Bump alphas

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -76,7 +76,7 @@ local fakeBWVersion, fakeBWHash
 local bwVersionResponseString = "V^%d^%s"
 -- The string that is shown as version
 if isRetail then
-	DBM.DisplayVersion = "9.2.33"
+	DBM.DisplayVersion = "9.2.34 alpha"
 	DBM.ReleaseRevision = releaseDate(2022, 9, 7) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	fakeBWVersion, fakeBWHash = 243, "d58ab26"
 elseif isClassic then
@@ -88,7 +88,7 @@ elseif isBCC then
 	DBM.ReleaseRevision = releaseDate(2022, 8, 1) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	fakeBWVersion, fakeBWHash = 41, "287b8dd"
 elseif isWrath then
-	DBM.DisplayVersion = "3.4.9"
+	DBM.DisplayVersion = "3.4.10 alpha"
 	DBM.ReleaseRevision = releaseDate(2022, 9, 7) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	fakeBWVersion, fakeBWHash = 41, "287b8dd"
 end
