b7e5fae92bce02018e181fdf2267df169bbd0241
MysticalOS <mysticalosx@gmail.com>
2022-09-20 01:02:09 -0400

    prep new wrath tag

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -88,8 +88,8 @@ elseif isBCC then
 	DBM.ReleaseRevision = releaseDate(2022, 8, 1) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	fakeBWVersion, fakeBWHash = 41, "287b8dd"
 elseif isWrath then
-	DBM.DisplayVersion = "3.4.11 alpha"
-	DBM.ReleaseRevision = releaseDate(2022, 9, 14) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DBM.DisplayVersion = "3.4.11"
+	DBM.ReleaseRevision = releaseDate(2022, 9, 20) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	fakeBWVersion, fakeBWHash = 41, "287b8dd"
 end
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
