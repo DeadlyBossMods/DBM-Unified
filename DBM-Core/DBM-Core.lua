7293efbc0c820f7c3f9d341ca1ee95a351d345df
MysticalOS <mysticalosx@gmail.com>
2023-01-04 01:06:24 -0500

    Prep new tag for retail

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -77,8 +77,8 @@ local fakeBWVersion, fakeBWHash
 local bwVersionResponseString = "V^%d^%s"
 -- The string that is shown as version
 if isRetail then
-	DBM.DisplayVersion = "10.0.16 alpha"
-	DBM.ReleaseRevision = releaseDate(2023, 1, 1) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DBM.DisplayVersion = "10.0.16"
+	DBM.ReleaseRevision = releaseDate(2023, 1, 4) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	fakeBWVersion, fakeBWHash = 259, "3fbb48c"
 elseif isClassic then
 	DBM.DisplayVersion = "1.14.29 alpha"
