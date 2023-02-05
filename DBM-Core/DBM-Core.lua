3cd0af74ba482b3132fc9f5e0fdccf9758461592
MysticalOS <mysticalosx@gmail.com>
2023-02-05 02:38:28 -0500

    Move sort functions to core fixed a bug where icon sort methods using ranged would still throw lua errors

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -7264,7 +7264,7 @@ do
 	end
 	bossModPrototype.IsMelee = DBM.IsMelee
 
-	function bossModPrototype:IsRanged(uId)
+	function DBM:IsRanged(uId)
 		if uId then
 			local name = GetUnitName(uId, true)
 			if isRetail and raid[name].specID then--We know their specId
@@ -7280,6 +7280,7 @@ do
 		end
 		return private.specRoleTable[currentSpecID]["Ranged"]
 	end
+	bossModPrototype.IsRanged = DBM.IsRanged
 
 	function bossModPrototype:IsSpellCaster(uId)
 		if uId then
@@ -7313,6 +7314,97 @@ do
 			DBM:SetCurrentSpecInfo()
 		end
 		return private.specRoleTable[currentSpecID]["MagicDispeller"]
+	end
+
+	---------------------
+	--  Sort Methods  --
+	---------------------
+	function DBM:SortByGroup(v1, v2)
+		return self:GetGroupId(self:GetUnitFullName(v1), true) < self:GetGroupId(self:GetUnitFullName(v2), true)
+	end
+	function DBM:SortByTankAlpha(v1, v2)
+		--Tank > Melee > Ranged prio, and if two of any of types, alphabetical names are preferred
+		if self:IsTanking(v1) == self:IsTanking(v2) then
+			return self:GetUnitFullName(v1) < self:GetUnitFullName(v2)
+		--if one is tank and one isn't, they are not equal so it goes to the below elseifs that prio melee
+		elseif self:IsTanking(v1) and not self:IsTanking(v2) then
+			return true
+		elseif self:IsTanking(v2) and not self:IsTanking(v1) then
+			return false
+		elseif self:IsMelee(v1) == self:IsMelee(v2) then
+			return self:GetUnitFullName(v1) < self:GetUnitFullName(v2)
+		--if one is melee and one is ranged, they are not equal so it goes to the below elseifs that prio melee
+		elseif self:IsMelee(v1) and not self:IsMelee(v2) then
+			return true
+		elseif self:IsMelee(v2) and not self:IsMelee(v1) then
+			return false
+		end
+	end
+	function DBM:SortByTankRoster(v1, v2)
+		--Tank > Melee > Ranged prio, and if two of any of types, roster index as secondary
+		if self:IsTanking(v1) == self:IsTanking(v2) then
+			return self:GetGroupId(self:GetUnitFullName(v1), true) < self:GetGroupId(self:GetUnitFullName(v2), true)
+		--if one is melee and one is ranged, they are not equal so it goes to the below elseifs that prio melee
+		elseif self:IsTanking(v1) and not self:IsTanking(v2) then
+			return true
+		elseif self:IsTanking(v2) and not self:IsTanking(v1) then
+			return false
+		elseif self:IsMelee(v1) == self:IsMelee(v2) then
+			return self:GetGroupId(self:GetUnitFullName(v1), true) < self:GetGroupId(self:GetUnitFullName(v2), true)
+		--if one is melee and one is ranged, they are not equal so it goes to the below elseifs that prio melee
+		elseif self:IsMelee(v1) and not self:IsMelee(v2) then
+			return true
+		elseif self:IsMelee(v2) and not self:IsMelee(v1) then
+			return false
+		end
+	end
+	function DBM:SortByMeleeAlpha(v1, v2)
+		--if both are melee, the return values are equal and we use alpha sort
+		--if both are ranged, the return values are equal and we use alpha sort
+		if self:IsMelee(v1) == self:IsMelee(v2) then
+			return self:GetUnitFullName(v1) < self:GetUnitFullName(v2)
+		--if one is melee and one is ranged, they are not equal so it goes to the below elseifs that prio melee
+		elseif self:IsMelee(v1) and not self:IsMelee(v2) then
+			return true
+		elseif self:IsMelee(v2) and not self:IsMelee(v1) then
+			return false
+		end
+	end
+	function DBM:SortByMeleeRoster(v1, v2)
+		--if both are melee, the return values are equal and we use raid roster index sort
+		--if both are ranged, the return values are equal and we use raid roster index sort
+		if self:IsMelee(v1) == self:IsMelee(v2) then
+			return self:GetGroupId(self:GetUnitFullName(v1), true) < self:GetGroupId(self:GetUnitFullName(v2), true)
+		--if one is melee and one is ranged, they are not equal so it goes to the below elseifs that prio melee
+		elseif self:IsMelee(v1) and not self:IsMelee(v2) then
+			return true
+		elseif self:IsMelee(v2) and not self:IsMelee(v1) then
+			return false
+		end
+	end
+	function DBM:SortByRangedAlpha(v1, v2)
+		--if both are melee, the return values are equal and we use alpha sort
+		--if both are ranged, the return values are equal and we use alpha sort
+		if self:IsRanged(v1) == self:IsRanged(v2) then
+			return self:GetUnitFullName(v1) < self:GetUnitFullName(v2)
+		--if one is melee and one is ranged, they are not equal so it goes to the below elseifs that prio melee
+		elseif self:IsRanged(v1) and not self:IsRanged(v2) then
+			return true
+		elseif self:IsRanged(v2) and not self:IsRanged(v1) then
+			return false
+		end
+	end
+	function DBM:SortByRangedRoster(v1, v2)
+		--if both are melee, the return values are equal and we use raid roster index sort
+		--if both are ranged, the return values are equal and we use raid roster index sort
+		if self:IsRanged(v1) == self:IsRanged(v2) then
+			return self:GetGroupId(self:GetUnitFullName(v1), true) < self:GetGroupId(self:GetUnitFullName(v2), true)
+		--if one is melee and one is ranged, they are not equal so it goes to the below elseifs that prio melee
+		elseif self:IsRanged(v1) and not self:IsRanged(v2) then
+			return true
+		elseif self:IsRanged(v2) and not self:IsRanged(v1) then
+			return false
+		end
 	end
 end
 
