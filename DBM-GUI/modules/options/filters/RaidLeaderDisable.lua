43949d0b966ca051ac27bf47fd4d2e56cc6acd28
MysticalOS <mysticalosx@gmail.com>
2023-02-05 18:43:50 -0500

    fix gui alignment issues

--- DBM-GUI/modules/options/filters/RaidLeaderDisable.lua
+++ DBM-GUI/modules/options/filters/RaidLeaderDisable.lua
@@ -12,8 +12,8 @@ configOverrideArea:CreateCheckButton(L.OverrideBossAnnounceOptions, true, nil, "
 configOverrideArea:CreateCheckButton(L.OverrideBossTimerOptions, true, nil, "OverrideBossTimer")
 configOverrideArea:CreateCheckButton(L.OverrideBossIconOptions, true, nil, "OverrideBossIcon")
 configOverrideArea:CreateCheckButton(L.OverrideBossSayOptions, true, nil, "OverrideBossSay")
-local infotext = configOverrideArea:CreateText(L.ConfigAreaFooter, 380, false, GameFontNormalSmall, "LEFT", 0)
-infotext:SetPoint("BOTTOMLEFT", featureOverridesArea.frame, "BOTTOMLEFT", 10, 10)
+local infotext = configOverrideArea:CreateText(L.ConfigAreaFooter, nil, false, GameFontNormalSmall, "LEFT", 25)
+infotext:SetPoint("BOTTOMLEFT", configOverrideArea.frame, "BOTTOMLEFT", 10, 10)
 
-local infotext2 = rlControlsPanel:CreateText(L.TabFooter, 380, false, GameFontNormalSmall, "LEFT", 0)
+local infotext2 = rlControlsPanel:CreateText(L.TabFooter, nil, false, GameFontNormalSmall, "LEFT", 0)
 infotext2:SetPoint("BOTTOMLEFT", rlControlsPanel.frame, "BOTTOMLEFT", 10, 10)
