d15317cebcd5b6d80f5bd516254c341be539c78a
Mitälie <16635602+Mitalie@users.noreply.github.com>
2023-06-25 10:32:32 +0300

    Create spellId / key mapping table
    Scrap per-mod spellId table. Per mod stage mapping is still supported
    but unused for now as Aberrus stages match.

--- DBM-Core/modules/BWCallbacks.lua
+++ DBM-Core/modules/BWCallbacks.lua
@@ -12,6 +12,57 @@ end
 -------------------------
 local GetBWStage, GetBWKey, GetBWModule
 do
+	local spellIdMap = {
+		-- Kazzara
+		[406516] = 407196,
+		[406525] = 407196,
+		[404744] = 404743,
+		-- The Amalgamation Chamber
+		[405016] = 405036,
+		[405641] = 405642,
+		-- The Forgotten Experiments
+		[405042] = 407327,
+		[405391] = 405392,
+		-- Assault of the Za'qali
+		["ej26217"] = 397383,
+		[410535] = 401516,
+		-- Zskarn
+		[404007] = 404010,
+		-- Magmorax
+		[402989] = 402994,
+		[409238] = 409093,
+		-- Echo of Neltharion
+		[402116] = 402115,
+		[406222] = 401998,
+		[404038] = 405433,
+		-- Scalecommander Sarkareth
+		[401642] = 401680,
+		[401325] = 401330,
+		[411236] = 411241,
+		[403517] = 403520,
+		[408422] = 408429,
+		[405022] = 405486,
+
+		-- The Primalist Council
+		[374021] = 372279,
+		[370991] = 397134,
+		[374043] = 374038,
+		-- Sennarth
+		["ej24899"] = -24899,
+		[373027] = 373048,
+		-- Kurog Grimtotem
+		[390548] = 372158,
+		[374022] = 374023,
+		[372456] = 372458,
+		[391055] = 391056,
+		-- Broodkeeper Diurna
+		[375842] = 380175,
+		[380176] = 392194,
+		-- Raszageth
+		[377658] = 395906,
+		[378829] = 377497,
+	}
+
 	function GetBWStage(mod)
 		local bwCompat = mod.bwCompat
 		local modStage = bwCompat and bwCompat.useStageTotality and (mod.vb.stageTotality or 0) or (mod.vb.phase or 0)
@@ -19,8 +70,7 @@ do
 	end
 
 	function GetBWKey(obj)
-		local bwCompat = obj.mod.bwCompat
-		return bwCompat and bwCompat.keys and (bwCompat.keys[obj] or bwCompat.keys[obj.spellId]) or obj.spellId
+		return spellIdMap[obj.spellId] or obj.spellId
 	end
 
 	local fakeBWModules = setmetatable({}, {
