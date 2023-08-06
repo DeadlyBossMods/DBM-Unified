08166b43c2f031de988597ffaab21c5fd74f3b75
Artemis <QartemisT@gmail.com>
2023-08-06 18:51:11 +0100

    Expose colorVariables in DBT

--- DBM-StatusBarTimers/DBT.lua
+++ DBM-StatusBarTimers/DBT.lua
@@ -744,6 +744,7 @@ local colorVariables = {
 	[7] = "UI",--Important 1
 	[8] = "I2",--Important 2
 }
+DBT.colorVariables = colorVariables
 
 local function stringFromTimer(t)
 	if t <= DBT.Options.TDecimal then
