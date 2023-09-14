5c18da91dd8875c5a1321c3650adbb412e9928ee
MysticalOS <mysticalosx@gmail.com>
2023-09-14 07:52:23 -0400

    prep new tag for retail

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -80,8 +80,8 @@ local bwVersionResponseString = "V^%d^%s"
 local PForceDisable
 -- The string that is shown as version
 if isRetail then
-	DBM.DisplayVersion = "10.1.27 alpha"
-	DBM.ReleaseRevision = releaseDate(2023, 9, 5) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DBM.DisplayVersion = "10.1.27"
+	DBM.ReleaseRevision = releaseDate(2023, 9, 14) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	PForceDisable = 6--When this is incremented, trigger force disable regardless of major patch
 elseif isClassic then
 	DBM.DisplayVersion = "1.14.47 alpha"
