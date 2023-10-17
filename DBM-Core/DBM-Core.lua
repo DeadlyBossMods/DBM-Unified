67ab118e9270d9013d76548f664dc76703bb12b1
MysticalOS <mysticalosx@gmail.com>
2023-10-17 16:21:02 -0400

    prevent precise object from breaking if less people are alive or present than the expected max by still using scheduling fallback

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -8675,7 +8675,7 @@ do
 		if total == #self.combinedtext then--All targets gathered, show immediately
 			self:Show(...)--Does this need self or mod? will it have this bug? https://github.com/DeadlyBossMods/DBM-Unified/issues/153
 		else
-			DBMScheduler:Schedule(delay or 0.5, self.Show, self.mod, self, ...)
+			DBMScheduler:Schedule(1, self.Show, self.mod, self, ...)
 		end
 	end
 
@@ -9592,7 +9592,7 @@ do
 		if total == #self.combinedtext then--All targets gathered, show immediately
 			self:Show(...)--Does this need self or mod? will it have this bug? https://github.com/DeadlyBossMods/DBM-Unified/issues/153
 		else
-			DBMScheduler:Schedule(delay or 0.5, self.Show, self.mod, self, ...)
+			DBMScheduler:Schedule(1, self.Show, self.mod, self, ...)
 		end
 	end
 
