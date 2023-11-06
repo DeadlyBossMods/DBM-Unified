eba6ac1712e8fe26957281f8d8a42cae15a9fcbc
BlueNightSky <afan1128@hotmail.com>
2023-11-06 16:41:48 +0800

    Update localization.tw.lua (GUI)

--- DBM-GUI/localization.tw.lua
+++ DBM-GUI/localization.tw.lua
@@ -85,7 +85,7 @@ L.Editbox_WindowWidth		= "GUI視窗寬度"
 L.Editbox_WindowHeight		= "GUI視窗高度"
 
 L.UIGroupingOptions			= "界面分組選項 (更改這些需要輸入 /reload 來重載界面)"
-L.GroupOptionsExcludeIcon	= "按照技能分組排除“設置標記圖示”選項 (它們將像以前一樣在“圖示”類中顯示)"
+L.GroupOptionsExcludeIcon	= "按照技能分組排除“設置標記圖示”選項 (它們將一起分類為為自己的“圖示”類別)"
 L.AutoExpandSpellGroups		= "按照技能分組自動擴展選項"
 L.ShowWAKeys				= "在法術名稱旁顯示WeakAuras鍵來使用首領模組觸發器協助編寫WeakAuras。"
 --L.ShowSpellDescWhenExpanded	= "分組擴展時繼續顯示技能描述"
