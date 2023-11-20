a4744ad9c4dec8a3d0169fc845da9af2dffcbfee
MysticalOS <mysticalosx@gmail.com>
2023-11-20 12:17:52 -0500

    prep to remake 10.2.6 tag

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -81,7 +81,7 @@ local bwVersionResponseString = "V^%d^%s"
 local PForceDisable
 -- The string that is shown as version
 if isRetail then
-	DBM.DisplayVersion = "10.2.7 alpha"
+	DBM.DisplayVersion = "10.2.6"
 	DBM.ReleaseRevision = releaseDate(2023, 11, 20) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	PForceDisable = 8--When this is incremented, trigger force disable regardless of major patch
 elseif isClassic then
