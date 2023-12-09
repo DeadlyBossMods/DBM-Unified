f57f15b4651c758085ff1493211365241ed37e4d
MysticalOS <mysticalosx@gmail.com>
2023-12-09 16:26:24 -0500

    prep new classic era tag

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -85,8 +85,8 @@ if isRetail then
 	DBM.ReleaseRevision = releaseDate(2023, 12, 8) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	PForceDisable = 8--When this is incremented, trigger force disable regardless of major patch
 elseif isClassic then
-	DBM.DisplayVersion = "1.15.4 alpha"
-	DBM.ReleaseRevision = releaseDate(2023, 12, 8) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DBM.DisplayVersion = "1.15.4"
+	DBM.ReleaseRevision = releaseDate(2023, 12, 9) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	PForceDisable = 4--When this is incremented, trigger force disable regardless of major patch
 elseif isBCC then
 	DBM.DisplayVersion = "2.6.0 alpha"--When TBC returns (and it will one day). It'll probably be game version 2.6
