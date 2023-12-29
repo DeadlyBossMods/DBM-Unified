e5acdc8369df3417d4f6b8c66734503d6e48806e
MysticalOS <mysticalosx@gmail.com>
2023-12-29 18:15:31 -0500

    bump alpha

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -86,7 +86,7 @@ if isRetail then
 	DBM.ReleaseRevision = releaseDate(2023, 12, 27) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	PForceDisable = 8--When this is incremented, trigger force disable regardless of major patch
 elseif isClassic then
-	DBM.DisplayVersion = "1.15.6"
+	DBM.DisplayVersion = "1.15.7 alpha"
 	DBM.ReleaseRevision = releaseDate(2023, 12, 29) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	PForceDisable = 4--When this is incremented, trigger force disable regardless of major patch
 elseif isBCC then
