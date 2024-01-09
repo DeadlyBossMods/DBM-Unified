15ddc8d099d98652df176fe5e9d7ec01a3b6edef
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-01-09 13:42:46 -0500

    Update localization.br.lua (#364)

--- DBM-GUI/localization.br.lua
+++ DBM-GUI/localization.br.lua
@@ -6,21 +6,21 @@ local L = DBM_GUI_L
 
 L.MainFrame = "Deadly Boss Mods"
 
---L.TranslationByPrefix			= "Translated by "
-L.TranslationBy 				= "GlitterStorm @ Azralon" -- your name here, localizers!
---L.Website						= "Visit us on discord at |cFF73C2FBhttps://discord.gg/deadlybossmods|r. Follow on most socials @deadlybossmods or @mysticalos"
---L.WebsiteButton				= "Website"
+L.TranslationByPrefix		= "Traduzido por "
+L.TranslationBy 			= "GlitterStorm @ Azralon" -- your name here, localizers!
+L.Website					= "Visite-nos no discord em |cFF73C2FBhttps://discord.gg/deadlybossmods|r. Siga na maioria das redes sociais @deadlybossmods ou @mysticalos"
+L.WebsiteButton				= "Site"
 
-L.OTabBosses	= "Chefes"
---L.OTabRaids					= "Raids"--Just pve raids
---L.OTabDungeons				= "Dungeons"--Just dungeons
---L.OTabWorld					= "World Bosses"--Since there are so many world mods, enough to get their own tab
---L.OTabScenarios				= "Scenarios"--Future use, will be used for scenarios and delves, likely after there are more than 2 mods (so probably 12.x or later)
---L.OTabPlugins					= "Other"--Scenarios, PVP, Delves (11.x), Solo/Challenge content (torghast, mage tower, etc)
-L.OTabOptions	= "Opções"
---L.OTabAbout					= "About"
+L.OTabBosses				= "Chefes"
+L.OTabRaids					= "Raides"--Just pve raids
+L.OTabDungeons				= "Masmorras"--Just dungeons
+L.OTabWorld					= "Chefes do mundo"--Since there are so many world mods, enough to get their own tab
+L.OTabScenarios				= "Cenários"--Future use, will be used for scenarios and delves, likely after there are more than 2 mods (so probably 12.x or later)
+L.OTabPlugins				= "Outro"--Scenarios, PVP, Delves (11.x), Solo/Challenge content (torghast, mage tower, etc)
+L.OTabOptions				= "Opções"
+L.OTabAbout					= "Sobre"
 
---L.TabCategory_CURRENT_SEASON	= "Current Season"
+L.TabCategory_CURRENT_SEASON	= "Temporada atual"
 
 L.TabCategory_OTHER			= "Outros módulos"
 --L.TabCategory_AFFIXES		= "Affixes"
@@ -40,7 +40,7 @@ L.Button_LoadMod 			= "Carregar AddOn"
 L.Mod_Enabled				= "Habilitar: %s"
 L.Mod_Reset					= "Carregar opções padrão"
 L.Reset 					= "Resetar"
---L.Import					= "Import"
+L.Import					= "Importar"
 
 L.Enable					= "Habilitar"
 L.Disable					= "Desabilitar"
@@ -58,25 +58,25 @@ L.Statistic_BestKill		= "Melhor tempo:"
 
 -- Tab: General Options
 L.TabCategory_Options		= "Opções Gerais"
---L.Area_BasicSetup			= "Initial DBM Setup Tips"
---L.Area_ModulesForYou		= "What DBM modules are right for you?"
---L.Area_ProfilesSetup		= "DBM Profiles usage guide"
+L.Area_BasicSetup			= "Dicas iniciais de configuração do DBM"
+L.Area_ModulesForYou		= "Quais módulos DBM são adequados para você?"
+L.Area_ProfilesSetup		= "Guia de uso de perfis DBM"
 -- Panel: Core & GUI
---L.Core_GUI 				= "Core & GUI"
+L.Core_GUI 					= "Cor e GUI"
 L.General 					= "Opções gerais do DBM"
 L.EnableMiniMapIcon			= "Exibir botão no mini-mapa"
---L.UseSoundChannel			= "Set audio channel used by DBM to play alert sounds"
+L.UseSoundChannel			= "Defina o canal de áudio usado pelo DBM para reproduzir sons de alerta"
 L.UseMasterVolume			= "Utilizar canal principal de áudio para reproduzir arquivos de som."--Update
---L.UseDialogChannel		= "Dialog audio channel."
---L.UseSFXChannel			= "Sound Effects (SFX) audio channel."
+L.UseDialogChannel			= "Canal de áudio de diálogo."
+L.UseSFXChannel				= "Canal de áudio de efeitos sonoros (SFX)."
 L.Latency_Text				= "Definir latência máxima de sincronização: %d"
 
 L.Button_RangeFrame			= "Exibir/esconder quadro de distância"
 L.Button_RangeRadar			= "Exibir/esconder radar de distância" -- Não existe no en.lua
 L.Button_InfoFrame			= "Exibir/esconder quadro de informações"
 L.Button_TestBars			= "Iniciar barras de teste"
---L.Button_MoveBars			= "Move bars"
---L.Button_ResetInfoRange	= "Reset Info/Range frames"
+L.Button_MoveBars			= "Mover barras"
+L.Button_ResetInfoRange		= "Resetar quadros de informações/distância"
 
 L.ModelOptions				= "Opções do Visualizador de Modelos 3D"
 L.EnableModels				= "Habilitar modelos 3D nas opções de chefe"
@@ -84,19 +84,19 @@ L.ModelSoundOptions			= "Definir opção de som para o visualizador de modelos"
 L.ModelSoundShort			= "Curto"
 L.ModelSoundLong			= "Longo"
 
---L.ResizeOptions			= "Resize Options"
---L.ResizeInfo				= "You can resize the GUI by clicking the bottom-right corner, and dragging."
---L.Button_ResetWindowSize	= "Reset GUI window size"
---L.Editbox_WindowWidth		= "GUI window width"
---L.Editbox_WindowHeight	= "GUI window height"
+L.ResizeOptions				= "Opções de redimensionamento"
+L.ResizeInfo				= "Você pode redimensionar a GUI clicando no canto inferior direito e arrastando."
+L.Button_ResetWindowSize	= "Resetar o tamanho da janela da GUI"
+L.Editbox_WindowWidth		= "Largura da janela GUI"
+L.Editbox_WindowHeight		= "Altura da janela GUI"
 
---L.UIGroupingOptions					= "UI Grouping Options (changing these require UI reload for any mod that's already loaded)"
---L.GroupOptionsExcludeIcon			= "Exclude 'Set Icon on' options from getting grouped by spell (they will be grouped together in their own 'Icons' category instead)"
---L.AutoExpandSpellGroups				= "Auto expand options that are grouped by spell"
---L.ShowWAKeys						= "Show WeakAuras keys next to spell names to assist in writing WeakAuras using Boss Mod triggers."
---L.ShowSpellDescWhenExpanded	= "Continue showing spell description when groups are expanded"--Might not be used
---L.NoDescription						= "This ability has no description"
---L.CustomOptions						= "This category contains custom options for an ability or event that has no spell or journal ID of it's own. These options have been grouped together using a custom manual ID for the ease of creating WeakAuras"
+L.UIGroupingOptions			= "Opções de agrupamento de IU (alterá-las requer recarregamento da IU para qualquer mod que já esteja carregado)"
+L.GroupOptionsExcludeIcon	= "Exclua as opções de 'Ativar ícone' de serem agrupadas por feitiço (elas serão agrupadas em sua própria categoria de 'Ícones')"
+L.AutoExpandSpellGroups		= "Expanda automaticamente as opções agrupadas por feitiço"
+L.ShowWAKeys				= "Mostre as teclas WeakAuras ao lado dos nomes dos feitiços para ajudar a escrever WeakAuras usando os gatilhos do Boss Mod."
+L.ShowSpellDescWhenExpanded	= "Continuar mostrando a descrição do feitiço quando os grupos forem expandidos"--Might not be used
+L.NoDescription				= "Esta habilidade não tem descrição"
+L.CustomOptions				= "Esta categoria contém opções personalizadas para uma habilidade ou evento que não possui ID de feitiço ou diário próprio. Essas opções foram agrupadas usando um ID manual personalizado para facilitar a criação de WeakAuras"
 
 -- Tab: General Timer Options
 L.TimerGeneral 				= "Opções gerais do temporizador do DBM"
@@ -106,14 +106,14 @@ L.ChallengeTimerPersonal	= "Pessoal"
 L.ChallengeTimerRealm		= "Reino"
 
 -- Tab: Raidwarning
-L.Tab_RaidWarning 			= "Avisos de Raid"
-L.RaidWarning_Header		= "Opções de Avisos de Raid"
-L.RaidWarnColors 			= "Cores dos Avisos de Raid"
+L.Tab_RaidWarning 			= "Avisos de Raide"
+L.RaidWarning_Header		= "Opções de Avisos de Raide"
+L.RaidWarnColors 			= "Cores dos Avisos de Raide"
 L.RaidWarnColor_1 			= "Cor 1"
 L.RaidWarnColor_2 			= "Cor 2"
 L.RaidWarnColor_3		 	= "Cor 3"
 L.RaidWarnColor_4 			= "Cor 4"
-L.InfoRaidWarning			= [[Você pode especificar a cor e posição do quadro de avisos de raid.
+L.InfoRaidWarning			= [[Você pode especificar a cor e posição do quadro de avisos de raide.
 Esse quadro é utilizado para mensagens como "Jogador X está sob efeito de Y".]]
 L.ColorResetted 			= "As opções de cor desse campo foram resetadas."
 L.ShowWarningsInChat 		= "Exibir avisos na janela de chat."
@@ -128,12 +128,12 @@ L.Outline					= "Contorno"
 L.ThickOutline				= "Espessura do contorno"
 L.MonochromeOutline			= "Contorno Monocromático"
 L.MonochromeThickOutline	= "Espessura do contorno Monocromático"
-L.RaidWarnSound				= "toque som no aviso de raid"
+L.RaidWarnSound				= "toque som no aviso de raide"
 
 -- não existem no en.lua
 L.RaidWarnMessage 			= "Obrigado por utilizar o Deadly Boss Mods" -- não existe no en.lua
-L.BarWhileMove 				= "Aviso de Raid móvel" -- não existe no en.lua
-L.RaidWarnSound				= "Tocar som junto com o aviso na raid" -- não existe no en.lua
+L.BarWhileMove 				= "Aviso de Raide móvel" -- não existe no en.lua
+L.RaidWarnSound				= "Tocar som junto com o aviso na raide" -- não existe no en.lua
 
 
 
@@ -177,11 +177,11 @@ L.Bar_Decimal				= "Exibir porcentagens abaixo do temporizador: %d"
 L.Bar_EnlargeTime			= "Barras aumentam abaixo deste tempo: %d"
 L.BarSpark					= "Barra faísca"
 L.BarFlash					= "Barra pisca quando estiver para expirar"
-L.BarSort					= "selecionar pelo tempo restante"
+L.BarSort					= "Selecionar pelo tempo restante"
 
 
 -- Tab: Spec Warn Frame
-L.Panel_SpecWarnFrame		= "Avisos Raid Especiais"
+L.Panel_SpecWarnFrame		= "Avisos Raide Especiais"
 L.Area_SpecWarn				= "Opções de Avisos Especiais"
 L.ShowSWarningsInChat 		= "Exibir avisos especiais no quadro de chat"
 L.SpecWarn_FlashFrameRepeat	= "Repetir %d vezes (se habilitado)"
@@ -205,7 +205,7 @@ L.SpecWarn_NoSoundsWVoice	= "Filtrar avisos sonoros especiais para avisos que co
 L.SWFNever					= "Nunca"
 L.SWFDefaultOnly			= "Quando avisos especiais usarem som padrão ( permitir que sons customizados ainda toquem)."
 L.SWFAll					= "Quando sons especiais usarem qualquer som"
-L.SpecWarn_AlwaysVoice		= "Sempre tocar todos os avisos falados (Substitui opções especificas dos chefes. Útil para lideres de raids)"
+L.SpecWarn_AlwaysVoice		= "Sempre tocar todos os avisos falados (Substitui opções especificas dos chefes. Útil para lideres de raides)"
 --TODO, maybe add URLS right to GUI panel on where to acquire 3rd party voice packs? talvez ...
 
 
@@ -214,7 +214,7 @@ L.Panel_SpamFilter			= "Desligamentos globais & filtros"
 
 L.SpamBlockNoShowAnnounce	= "Não exibir anúncios ou tocar avisos sonoros"
 L.SpamBlockNoSetIcon		= "Não marcar jogadores com ícones"
-L.SpamBlockNoRangeFrame		= "Não exibir quadro de distancia"
+L.SpamBlockNoRangeFrame		= "Não exibir quadro de distância"
 L.SpamBlockNoInfoFrame		= "Não exibir quadro de informação"
 L.SpamBlockNoHudMap			= "Não mostrar mapas HudMap (heads up display Map)"
 L.SpamBlockNoCountdowns		= "Não tocar sons de contagem regressiva"
@@ -243,7 +243,7 @@ L.PT_Threshold				= "Não exibir o texto da contagem regressiva de puxada/custom
 
 L.Panel_HideBlizzard		= "Esconder Blizzard"
 L.Area_HideBlizzard			= "Esconder Opções da Blizzard"
-L.HideBossEmoteFrame		= "Esconder o quadro de emote do chefe de raid durante as lutas"
+L.HideBossEmoteFrame		= "Esconder o quadro de emote do chefe de raide durante as lutas"
 L.HideWatchFrame			= "Esconder/acompanhar quadros (de objetivos) durante as lutas contra os chefes, caso nenhuma conquista esteja sendo acompanhada. No modo desafio, cria uma medalha temporária na luta de chefes enquanto o quadro estiver escondido"
 L.HideGarrisonUpdates		= "Esconde o quadro de missão completada da guarnição enquanto estiver lutando com algum chefe"
 L.HideGuildChallengeUpdates	= "Esconde o quadro de desafio de guilda durante a luta contra algum chefe"
@@ -255,7 +255,7 @@ L.AfterFirst				= "Depois que o vídeo for assistido uma vez"
 
 L.Panel_ExtraFeatures		= "Características Extra"
 L.Area_ChatAlerts			= "Opções de alerta do Chat"
-L.RoleSpecAlert				= "Exibir mensagem de alerta quando sua especialização de saque não corresponder à sua especialização atual ao entrar na raid"
+L.RoleSpecAlert				= "Exibir mensagem de alerta quando sua especialização de saque não corresponder à sua especialização atual ao entrar na raide"
 L.CheckGear					= "envia um mensagem de alerta quando o seu ilvl equipado for muito menor do que o ilvl nas bolsas(40+)"
 L.WorldBossAlert			= "Exibir mensagem de alerta quando um chefe do mundo possivelmente for atacado no seu reino por membros da sua guilda ou amigos (inaccurate if sender is CRZed)"
 L.Area_SoundAlerts			= "Opções de Som de Alerta"
@@ -265,7 +265,7 @@ L.AFKHealthWarning			= "Tocar som de alerta quando você estiver perdendo vida e
 L.Area_AutoLogging			= "opções de relatórios automáticos"
 L.AutologBosses				= "Gravar automaticamente o relatório de encontro com os chefes utilizando o relatório de combate da Blizzard(Requer que seja usado /dbm pull antes do chefe <a href=\"http://www.warcraftlogs.com\">|cff3588ffwarcraftlogs.com|r</a> compatibilidade)"
 L.AdvancedAutologBosses		= "Gravar automaticamente o encontro com o chefe utilizando o Transcritor"
-L.LogOnlyNonTrivial			= "Só gravar encontros com chefes de raid (exclui Localizador de Raids/Grupos/Cenários)"
+L.LogOnlyNonTrivial			= "Só gravar encontros com chefes de raide (exclui Localizador de Raides/Grupos/Cenários)"
 L.Area_3rdParty				= "opções de add0ns de terceiros"
 --L.oRA3AnnounceConsumables	= "Announce oRA3 consumables check on combat start"
 L.Area_Invite				= "Opções de convite"
