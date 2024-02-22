7206b5181da8a2f3f6ce18adaedaf59ab44e240e
MysticalOS <mysticalosx@gmail.com>
2024-02-22 00:59:16 -0500

    Bump versions again and make it a mandatory (force disable) triggering update do to how important bug fix is since the function that was broken is used A LOT by a lot of mods and core functions, especially affecting wrath classic but also retail and classic era in certain situations custom "boss unit Id only" checks were used.
    Long short of it. In this weeks updates, a function was rewritten to be better but had a bug in it that broke the rewrite in specific circomstances that were missed in testing because I didn't do the testing on wrath classic (the only client that has yet to recieve UnitTokenFromGUID from blizzard.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -84,21 +84,21 @@ local bwVersionResponseString = "V^%d^%s"
 local PForceDisable
 -- The string that is shown as version
 if isRetail then
-	DBM.DisplayVersion = "10.2.26 alpha"
-	DBM.ReleaseRevision = releaseDate(2024, 2, 21) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
-	PForceDisable = 9--When this is incremented, trigger force disable regardless of major patch
+	DBM.DisplayVersion = "10.2.26"
+	DBM.ReleaseRevision = releaseDate(2024, 2, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	PForceDisable = 10--When this is incremented, trigger force disable regardless of major patch
 elseif isClassic then
-	DBM.DisplayVersion = "1.15.18 alpha"
-	DBM.ReleaseRevision = releaseDate(2024, 2, 21) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
-	PForceDisable = 5--When this is incremented, trigger force disable regardless of major patch
+	DBM.DisplayVersion = "1.15.18"
+	DBM.ReleaseRevision = releaseDate(2024, 2, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	PForceDisable = 6--When this is incremented, trigger force disable regardless of major patch
 elseif isBCC then
 	DBM.DisplayVersion = "2.6.0 alpha"--When TBC returns (and it will one day). It'll probably be game version 2.6
 	DBM.ReleaseRevision = releaseDate(2024, 1, 9) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
-	PForceDisable = 2--When this is incremented, trigger force disable regardless of major patch
+	PForceDisable = 3--When this is incremented, trigger force disable regardless of major patch
 elseif isWrath then
-	DBM.DisplayVersion = "3.4.61 alpha"
-	DBM.ReleaseRevision = releaseDate(2024, 2, 21) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
-	PForceDisable = 4--When this is incremented, trigger force disable regardless of major patch
+	DBM.DisplayVersion = "3.4.61"
+	DBM.ReleaseRevision = releaseDate(2024, 2, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	PForceDisable = 5--When this is incremented, trigger force disable regardless of major patch
 end
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
