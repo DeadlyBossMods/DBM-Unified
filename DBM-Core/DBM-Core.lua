4910e94f47c5c4bc0d4bcc22322fe44b0d81ca46
MysticalOS <mysticalosx@gmail.com>
2024-02-23 01:32:46 -0500

    bump retail for council bugfixes

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -84,8 +84,8 @@ local bwVersionResponseString = "V^%d^%s"
 local PForceDisable
 -- The string that is shown as version
 if isRetail then
-	DBM.DisplayVersion = "10.2.27 alpha"
-	DBM.ReleaseRevision = releaseDate(2024, 2, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DBM.DisplayVersion = "10.2.27"
+	DBM.ReleaseRevision = releaseDate(2024, 2, 23) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 	PForceDisable = 10--When this is incremented, trigger force disable regardless of major patch
 elseif isClassic then
 	DBM.DisplayVersion = "1.15.19 alpha"
