4c05eb984a2d50d3c06253d96d13395e5f5af3b9
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2024-03-15 18:25:38 +0500

    Update localization.ru.lua
    Small changed

--- DBM-Core/localization.ru.lua
+++ DBM-Core/localization.ru.lua
@@ -574,9 +574,9 @@ L.AUTO_RRANGE_OPTION_TEXT			= "Показывать обратное окно п
 L.AUTO_RRANGE_OPTION_TEXT_SHORT		= "Показывать обратное окно проверки дистанции (%s)"
 L.AUTO_INFO_FRAME_OPTION_TEXT		= "Показывать информационное окно для $spell:%s"
 L.AUTO_INFO_FRAME_OPTION_TEXT2		= "Показывать информационное окно для обзора боя"
-L.AUTO_INFO_FRAME_OPTION_TEXT3		= "Показать информационный фрейм для $spell:%s (при достижении порогового значения %%s)"
-L.AUTO_READY_CHECK_OPTION_TEXT		= "Проигрывать звук проверки готовности, когда пулят босса (даже если он не является целью)"
-L.AUTO_SPEEDCLEAR_OPTION_TEXT		= "Показать таймер для быстрой зачистки %s"
+L.AUTO_INFO_FRAME_OPTION_TEXT3		= "Показывать информационный фрейм для $spell:%s (при достижении порогового значения %%s)"
+L.AUTO_READY_CHECK_OPTION_TEXT		= "Воспроизводить звук проверки готовности, когда пуллят босса (даже если он не является целью)"
+L.AUTO_SPEEDCLEAR_OPTION_TEXT		= "Показывать таймер для быстрой зачистки %s"
 L.AUTO_PRIVATEAURA_OPTION_TEXT		= "Воспроизводить звуковые оповещения DBM для приватных аур $spell:%s в этом бою."
 
 -- New special warnings
